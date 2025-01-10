458c8cde5bd3a251f403c18d7c212ccac71a5a8b
Saurav <sauravks2010@gmail.com>
2025-01-10 07:47:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Careers from './pages/Careers';
 import Contact from './pages/Contact';
 import Privacy from './pages/Privacy';
 import BecomeADriver from './pages/BecomeADriver';
 import YourNextRide from './pages/YourNextRide';
 import ReactGA from 'react-ga4';
 import './styles/App.css';
+import { useEffect } from 'react';
 
 const TRACKING_ID = "G-93Q8JNK534";
 ReactGA.initialize(TRACKING_ID);
-ReactGA.send({ hitType: "pageview", page: "/", title: "Malama" });
+
+// Create a component to handle route changes
+function RouteTracker() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    ReactGA.send({ 
+      hitType: "pageview", 
+      page: location.pathname + location.search,
+      title: document.title
+    });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <RouteTracker />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -25,8 +40,6 @@ function App() {
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/become-a-driver" element={<BecomeADriver />} />
           <Route path="/your-next-ride" element={<YourNextRide />} />
-          {/* <Route path="/careers" element={<Careers />} /> */}
-
         </Routes>
       </Layout>
     </Router>
